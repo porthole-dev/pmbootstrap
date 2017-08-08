porthole-dev/pmbootstrap

commit 246f3a2172ea3afecc67f633318e1df03305ff00

Close #334: Check if pkgname is the aport folder name (#347)
...and remove an redundant sentence with broken grammar in another
error message.

pmb/parse/apkbuild.py
@@ -16,6 +16,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with pmbootstrap.  If not, see <http://www.gnu.org/licenses/>.
 """
+import os
+import logging
 import pmb.config
 
 
@@ -134,6 +136,15 @@ def apkbuild(args, path):
             else:
                 ret[attribute] = ""
 
+    # Sanity check: pkgname
+    suffix = "/" + ret["pkgname"] + "/APKBUILD"
+    if not os.path.realpath(path).endswith(suffix):
+        logging.info("Folder: '" + os.path.dirname(path) + "'")
+        logging.info("Pkgname: '" + ret["pkgname"] + "'")
+        raise RuntimeError("The pkgname must be equal to the name of"
+                           " the folder, that contains the APKBUILD!")
+
+    # Fill cache
     ret = replace_variables(ret)
     ret = cut_off_function_names(ret)
     args.cache["apkbuild"][path] = ret

pmb/parse/arguments.py
@@ -259,7 +259,6 @@ def arguments():
                              " forward. Simply enable it in pmb/config/__init__.py"
                              " in build_device_architectures, zap your package cache"
                              " (otherwise you will have issues with noarch packages)"
-                             " and try again. Some other things might break, but"
-                             " they should be easy to fix them.")
+                             " and try again.")
 
     return args

commit 3a08472eb778ba1776a18bcaf4159c75c329637d

Fix #349: Verify pkgname against folder name after replacing vars
This fixes a regression introduced in 246f3a2172ea3afecc67f633318e1df03305ff00.
Sorry for the inconvenience!

pmb/parse/apkbuild.py
@@ -128,13 +128,17 @@ def apkbuild(args, path):
                     value = []
             ret[attribute] = value
 
-    # Add missing entries
+    # Add missing keys
     for attribute, options in pmb.config.apkbuild_attributes.items():
         if attribute not in ret:
             if options["array"]:
                 ret[attribute] = []
             else:
                 ret[attribute] = ""
+
+    # Properly format values
+    ret = replace_variables(ret)
+    ret = cut_off_function_names(ret)
 
     # Sanity check: pkgname
     suffix = "/" + ret["pkgname"] + "/APKBUILD"
@@ -145,7 +149,5 @@ def apkbuild(args, path):
                            " the folder, that contains the APKBUILD!")
 
     # Fill cache
-    ret = replace_variables(ret)
-    ret = cut_off_function_names(ret)
     args.cache["apkbuild"][path] = ret
     return ret