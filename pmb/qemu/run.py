porthole-dev/pmbootstrap

commit 87f05afa8af1db0af8e2c8feafe33cf64645b27e

pmb.run.qemu.install_depends: add new depends (MR 2007)
Alpine's qemu packaging has been split up into more subpackages, so
install them too.

Related: https://gitlab.alpinelinux.org/alpine/aports/-/merge_requests/15554

pmb/qemu/run.py
@@ -238,8 +238,12 @@ def install_depends(args, arch):
         "qemu-audio-alsa",
         "qemu-audio-pa",
         "qemu-audio-sdl",
+        "qemu-hw-display-virtio-gpu",
+        "qemu-hw-display-virtio-gpu-pci",
+        "qemu-hw-display-virtio-vga",
         "qemu-system-" + arch,
         "qemu-ui-gtk",
+        "qemu-ui-opengl",
         "qemu-ui-sdl",
     ]
     pmb.chroot.apk.install(args, depends)

commit 4be2eebce02972230f16150ee7a46b17d8ea2655

pmb.run.qemu.install_depends: fix with v20.05 (MR 2009)
Don't try to install the recently split up packages if the pmaports
branch is based on Alpine 3.12.

Fixes: 87f05afa ("pmb.run.qemu.install_depends: add new depends (MR 2007)")

pmb/qemu/run.py
@@ -246,6 +246,15 @@ def install_depends(args, arch):
         "qemu-ui-opengl",
         "qemu-ui-sdl",
     ]
+
+    # QEMU packaging isn't split up as much in 3.12
+    channel_cfg = pmb.config.pmaports.read_config_channel(args)
+    if channel_cfg["branch_aports"] == "3.12-stable":
+        depends.remove("qemu-hw-display-virtio-gpu")
+        depends.remove("qemu-hw-display-virtio-gpu-pci")
+        depends.remove("qemu-hw-display-virtio-vga")
+        depends.remove("qemu-ui-opengl")
+
     pmb.chroot.apk.install(args, depends)
 
 