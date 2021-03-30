porthole-dev/pmbootstrap

commit 2185a1e96ea92fa8c1a84f3a68135e96f5d78939

menuconfig: use cross-compiler for configuration (MR 2023)
Not using the cross compiler for kconfig leads to some differences to
the actual compilation later.

pmb/build/menuconfig.py
@@ -91,9 +91,13 @@ def menuconfig(args, pkgname):
     aport = pmb.helpers.pmaports.find(args, pkgname)
     apkbuild = pmb.parse.apkbuild(args, aport + "/APKBUILD")
     arch = args.arch or get_arch(args, apkbuild)
+    suffix = pmb.build.autodetect.suffix(args, apkbuild, arch)
+    cross = pmb.build.autodetect.crosscompile(args, apkbuild, arch, suffix)
+    hostspec = pmb.parse.arch.alpine_to_hostspec(arch)
 
     # Set up build tools and makedepends
-    pmb.build.init(args)
+    pmb.build.init(args, suffix)
+    pmb.build.init_compiler(args, [], cross, arch)
     depends = apkbuild["makedepends"]
     kopt = "menuconfig"
     copy_xauth = False
@@ -128,7 +132,9 @@ def menuconfig(args, pkgname):
                     outputdir, output="tui",
                     env={"ARCH": pmb.parse.arch.alpine_to_kernel(arch),
                          "DISPLAY": os.environ.get("DISPLAY"),
-                         "XAUTHORITY": "/home/pmos/.Xauthority"})
+                         "XAUTHORITY": "/home/pmos/.Xauthority",
+                         "CROSS_COMPILE": hostspec + "-",
+                         "CC": hostspec + "-gcc"})
 
     # Find the updated config
     source = args.work + "/chroot_native" + outputdir + "/.config"

commit 2664963e549f7ea6a43ec1c3d0dd44de82cd21f6

pmb.build.menuconfig: fix for native arch case (MR 2041)
Don't try to install and use a cross compiler if none is needed.

Fixes: 2185a1e9 ("menuconfig: use cross-compiler for configuration (MR 2023)")

pmb/build/menuconfig.py
@@ -97,7 +97,8 @@ def menuconfig(args, pkgname):
 
     # Set up build tools and makedepends
     pmb.build.init(args, suffix)
-    pmb.build.init_compiler(args, [], cross, arch)
+    if cross:
+        pmb.build.init_compiler(args, [], cross, arch)
     depends = apkbuild["makedepends"]
     kopt = "menuconfig"
     copy_xauth = False
@@ -128,13 +129,14 @@ def menuconfig(args, pkgname):
     # Run make menuconfig
     outputdir = get_outputdir(args, pkgname, apkbuild)
     logging.info("(native) make " + kopt)
+    env = {"ARCH": pmb.parse.arch.alpine_to_kernel(arch),
+           "DISPLAY": os.environ.get("DISPLAY"),
+           "XAUTHORITY": "/home/pmos/.Xauthority"}
+    if cross:
+        env["CROSS_COMPILE"] = f"{hostspec}-"
+        env["CC"] = f"{hostspec}-gcc"
     pmb.chroot.user(args, ["make", kopt], "native",
-                    outputdir, output="tui",
-                    env={"ARCH": pmb.parse.arch.alpine_to_kernel(arch),
-                         "DISPLAY": os.environ.get("DISPLAY"),
-                         "XAUTHORITY": "/home/pmos/.Xauthority",
-                         "CROSS_COMPILE": hostspec + "-",
-                         "CC": hostspec + "-gcc"})
+                    outputdir, output="tui", env=env)
 
     # Find the updated config
     source = args.work + "/chroot_native" + outputdir + "/.config"