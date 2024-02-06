porthole-dev/pmbootstrap

commit 0b40f66c8d3480a174809302af7ff6426af00881

pmb: only enable abuild-apk wrapper for buildroot (MR 2246)
Some packages like kernels are cross compiled from the native chroot.
the apk_wrapper won't work here! So only enable it for buildroot chroots

Signed-off-by: Caleb Connolly <caleb@postmarketos.org>

pmb/build/init.py
@@ -61,7 +61,7 @@ def init(args, suffix="native"):
     apk_arch = pmb.parse.arch.from_chroot_suffix(args, suffix)
 
     # Add apk wrapper that runs native apk and lies about arch
-    if not os.path.exists(chroot + "/usr/local/bin/abuild-apk"):
+    if suffix.startswith("buildroot_") and not os.path.exists(chroot + "/usr/local/bin/abuild-apk"):
         with open(chroot + "/tmp/apk_wrapper.sh", "w") as handle:
             content = f"""
                 #!/bin/sh

commit 0a8581e8b898af4f39f1ba23450645668bbd187f

pmb.build.init: fix apk wrapper for x86 on x86_64 (MR 2247)
Only install the abuild-apk wrapper if cpu emulation is required. This
fixes building for x86 on x86_64.

Fix for:
  >>> postmarketos-base-ui-gnome: Analyzing dependencies...
  /usr/local/bin/abuild-apk: line 11: /native/usr/bin/abuild-apk: not found

Fixes: 0b40f66c ("pmb: only enable abuild-apk wrapper for buildroot (MR 2246)")

pmb/build/init.py
@@ -10,6 +10,7 @@ import pmb.config
 import pmb.chroot
 import pmb.chroot.apk
 import pmb.helpers.run
+import pmb.parse.arch
 
 
 def init_abuild_minimal(args, suffix="native"):
@@ -61,7 +62,8 @@ def init(args, suffix="native"):
     apk_arch = pmb.parse.arch.from_chroot_suffix(args, suffix)
 
     # Add apk wrapper that runs native apk and lies about arch
-    if suffix.startswith("buildroot_") and not os.path.exists(chroot + "/usr/local/bin/abuild-apk"):
+    if pmb.parse.arch.cpu_emulation_required(apk_arch) and \
+            not os.path.exists(chroot + "/usr/local/bin/abuild-apk"):
         with open(chroot + "/tmp/apk_wrapper.sh", "w") as handle:
             content = f"""
                 #!/bin/sh