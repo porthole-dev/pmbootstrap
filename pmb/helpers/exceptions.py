5d4702f5b6539c5539e6d36a9259a410796669da
Newbyte <newbyte@postmarketos.org>
2024-03-20 22:56:13 +0100

    pmb.helpers.exceptions: Introduce (MR 2279)
    Initially I thought of putting this in pmb/helpers/other.py, but this
    resulted in a circular import, so I thought the easiest way to work
    around that was to just create a new file.

---
+++ pmb/helpers/exceptions.py
@@ -0,0 +1,5 @@
+class NonBugError(Exception):
+    """Exception which originates from a problem not caused by pmbootstrap's code. This
+    could for example be raised if there is an error in a package pmboostrap is
+    interacting with in some way."""
+    pass
