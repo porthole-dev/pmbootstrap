7fb334bdc829dc7c39405cb346c6163d7c31173d
Newbyte <newbyte@postmarketos.org>
2024-03-28 00:19:16 +0100

    pmb.helpers.exceptions: Add licence header (MR 2284)
    I forgot to add this in the original commit.
    
    Fixes 5d4702f5b6539c5539e6d36a9259a410796669da

--- pmb/helpers/exceptions.py
+++ pmb/helpers/exceptions.py
@@ -1,3 +1,7 @@
+# Copyright 2024 Stefan "Newbyte" Hansson
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
 class NonBugError(Exception):
     """Exception which originates from a problem not caused by pmbootstrap's code. This
     could for example be raised if there is an error in a package pmboostrap is
