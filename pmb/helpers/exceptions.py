27c1caf3d2d0d40f4589ddd6d8b3b1af6db082f7
Newbyte <newbyte@postmarketos.org>
2024-04-03 09:38:13 +0200

    pmb.helpers.exceptions: Add BuildFailedError (MR 2288)

--- pmb/helpers/exceptions.py
+++ pmb/helpers/exceptions.py
@@ -2,6 +2,13 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 
+class BuildFailedError(Exception):
+    """Exception to be raised when pmbootstrap fails to build a package. This is handled
+    separately from NonBugError as it needs to be treated differently as we want to hint
+    to users that they can check the log for more information when a build fails."""
+    pass
+
+
 class NonBugError(Exception):
     """Exception which originates from a problem not caused by pmbootstrap's code. This
     could for example be raised if there is an error in a package pmboostrap is
