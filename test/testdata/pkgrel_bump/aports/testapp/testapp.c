6af916536677141c0e19ed84903006902ec0d3fc
Caleb Connolly <caleb@postmarketos.org>
2024-06-05 02:31:49 +0200

    test: drop all tests (MR 2252)
    (Commit message written by Oliver)
    
    The tests relied on a lot of specifics of the old codebase. We decided
    that it makes more sense to just remove all tests for now to get this
    large patchset of refactorings and improvements done, and then gradually
    add new tests / possibly adjust some of the old ones and bring them back
    in future patches.
    
    Future tests will not be in the "test" directory, Caleb's reasoning:
    > Easier to find relevant tests for a module, and avoid the big flat
    > list of test files like we had before. Honestly was just a vibes
    > thing after reading the docs.
    
    Signed-off-by: Caleb Connolly <caleb@postmarketos.org>
    Tweaked-By: Oliver Smith <ollieparanoid@postmarketos.org>

--- test/testdata/pkgrel_bump/aports/testapp/testapp.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <testlib.h>
-
-int main(int argc, char **argv) {
-    testlib_hello();
-    return 0;
-}
--- test/testdata/pkgrel_bump/aports/testlib/testlib.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void testlib_hello() {
-	printf("hello, world from testlib!\n");
-}
