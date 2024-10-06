ab127681b5a7739004bbbda138c6afd86d407732
Stefan Hansson <newbyte@postmarketos.org>
2024-10-06 13:03:48 +0200

    pmb.core.apkindex_block: Introduce (MR 2425)

---
+++ pmb/core/apkindex_block.py
@@ -0,0 +1,24 @@
+# Copyright 2024 Stefan Hansson
+# SPDX-License-Identifier: GPL-3.0-or-later
+from dataclasses import dataclass
+
+from pmb.core.arch import Arch
+
+
+@dataclass
+class ApkindexBlock:
+    """
+    "depends" is not set for packages without any dependencies, e.g. ``musl``.
+
+    "timestamp" and "origin" are not set for virtual packages (#1273).
+    We use that information to skip these virtual packages in parse().
+    """
+
+    arch: Arch
+    depends: list[str] | None
+    origin: str | None
+    pkgname: str
+    provides: list[str]
+    provider_priority: int | None
+    timestamp: str | None
+    version: str
