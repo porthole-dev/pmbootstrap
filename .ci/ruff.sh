aeddda00681910cb74bdb355c04abce9e23eb47d
Caleb Connolly <caleb@postmarketos.org>
2024-10-21 22:43:06 +0200

    CI: ruff: run ALLL the checks before failing (MR 2448)
    So we don't go through the pain of pushing 3 times to get 3 different
    ruff failures in CI.
    
    Signed-off-by: Caleb Connolly <caleb@postmarketos.org>

--- .ci/ruff.sh
+++ .ci/ruff.sh
@@ -8,15 +8,19 @@ if [ "$(id -u)" = 0 ]; then
 	exec su "${TESTUSER:-build}" -c "sh -e $0"
 fi
 
+DID_FAIL=0
+
 set -x
 
 # __init__.py with additional ignore:
 # F401: imported, but not used
 # shellcheck disable=SC2046
-ruff check --ignore "F401" $(find . -not -path '*/venv/*' -name '__init__.py')
+ruff check --ignore "F401" $(find . -not -path '*/venv/*' -name '__init__.py') || DID_FAIL=1
 
 # Check all other files
-ruff check --exclude=__init__.py .
+ruff check --exclude=__init__.py . || DID_FAIL=1
 
 # Check formatting
-ruff format --diff
+ruff format --diff || DID_FAIL=1
+
+exit $DID_FAIL
