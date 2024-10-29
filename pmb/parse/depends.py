44c262b410461e9a7d6b9a0867a730bb90ca659c
Newbyte <newbyte@postmarketos.org>
2024-10-29 15:38:32 +0100

    pmb.parse.depends: Remove unused functions (MR 2464)
    This is no longer used as of a86a890a2ee614cfce2e85e3bdee787975d13687,
    so just remove this dead code.

--- pmb/parse/depends.py
+++ pmb/parse/depends.py
@@ -9,26 +9,6 @@ from pmb.core import Chroot
 from pmb.core.context import get_context
 
 
-def package_from_aports(pkgname_depend):
-    """
-    :returns: None when there is no aport, or a dict with the keys pkgname,
-              depends, version. The version is the combined pkgver and pkgrel.
-    """
-    # Get the aport
-    aport = pmb.helpers.pmaports.find_optional(pkgname_depend)
-    if not aport:
-        return None
-
-    # Parse its version
-    apkbuild = pmb.parse.apkbuild(aport / "APKBUILD")
-    pkgname = apkbuild["pkgname"]
-    version = apkbuild["pkgver"] + "-r" + apkbuild["pkgrel"]
-
-    # Return the dict
-    logging.verbose(f"{pkgname_depend}: provided by: {pkgname}-{version} in {aport}")
-    return {"pkgname": pkgname, "depends": apkbuild["depends"], "version": version}
-
-
 def package_provider(
     pkgname, pkgnames_install, suffix: Chroot = Chroot.native()
 ) -> pmb.core.apkindex_block.ApkindexBlock | None:
@@ -90,31 +70,3 @@ def package_provider(
 
     # 7. Pick the shortest provider. (Note: Normally apk would fail here!)
     return pmb.parse.apkindex.provider_shortest(providers, pkgname)
-
-
-def package_from_index(
-    pkgname_depend, pkgnames_install, package_aport, suffix: Chroot = Chroot.native()
-):
-    """
-    :returns: None when there is no aport and no binary package, or a dict with
-              the keys pkgname, depends, version from either the aport or the
-              binary package provider.
-    """
-    # No binary package
-    provider = package_provider(pkgname_depend, pkgnames_install, suffix)
-    if not provider:
-        return package_aport
-
-    # Binary package outdated
-    if package_aport and pmb.parse.version.compare(package_aport["version"], provider.version) == 1:
-        logging.verbose(pkgname_depend + ": binary package is outdated")
-        return package_aport
-
-    # Binary up to date (#893: overrides aport, so we have sonames in depends)
-    if package_aport:
-        logging.verbose(
-            pkgname_depend + ": binary package is"
-            " up to date, using binary dependencies"
-            " instead of the ones from the aport"
-        )
-    return provider
