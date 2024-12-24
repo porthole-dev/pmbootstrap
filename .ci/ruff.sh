99bd4cb636707dff0b574e3e1d028367031e73b9
Newbyte <newbyte@postmarketos.org>
2024-12-24 12:58:05 +0100

    CI: Simplify Ruff check (MR 2517)
    As of 76640b5ce8756a03a8182d0bbfee14e975cb0339, we ignore F401 in
    __init__.py through the pyproject.toml configuration file, so we don't
    need to treat it differently here in the CI script too.

--- .ci/ruff.sh
+++ .ci/ruff.sh
@@ -12,13 +12,8 @@ DID_FAIL=0
 
 set -x
 
-# __init__.py with additional ignore:
-# F401: imported, but not used
-# shellcheck disable=SC2046
-ruff check --ignore "F401" $(find . -not -path '*/venv/*' -name '__init__.py') || DID_FAIL=1
-
-# Check all other files
-ruff check --exclude=__init__.py . || DID_FAIL=1
+# Lint all files
+ruff check || DID_FAIL=1
 
 # Check formatting
 ruff format --diff || DID_FAIL=1
