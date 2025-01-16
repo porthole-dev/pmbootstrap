3893399744545852137773742346930096e1bf46
Newbyte <newbyte@postmarketos.org>
2025-01-16 12:22:28 +0100

    pmb.helpers.ui: Add must_exist argument to check_option() (MR 2530)
    Will be used in the next commit.

--- pmb/helpers/ui.py
+++ pmb/helpers/ui.py
@@ -31,9 +31,13 @@ def list_ui(arch: Arch) -> list[tuple[str, str]]:
     return ret
 
 
-def check_option(ui: str, option: str, with_extra_repos: WithExtraRepos = "default") -> bool:
+def check_option(
+    ui: str, option: str, must_exist: bool = True, with_extra_repos: WithExtraRepos = "default"
+) -> bool:
     """
     Check if an option, such as pmb:systemd, is inside an UI's APKBUILD.
+
+    If must_exist is set to False, False will be returned if the UI doesn't exist.
     """
     if ui == "none":
         # Users can select "none" as UI in "pmbootstrap init", which does not
@@ -42,6 +46,6 @@ def check_option(ui: str, option: str, with_extra_repos: WithExtraRepos = "defau
 
     pkgname = f"postmarketos-ui-{ui}"
     apkbuild = pmb.helpers.pmaports.get(
-        pkgname, subpackages=False, with_extra_repos=with_extra_repos
+        pkgname, must_exist, subpackages=False, with_extra_repos=with_extra_repos
     )
-    return option in apkbuild["options"]
+    return option in apkbuild["options"] if apkbuild is not None else False
