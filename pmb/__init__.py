0a45bfc09503aa15165c6cb2533b84166dcc79c5
Oliver Smith <ollieparanoid@postmarketos.org>
2025-02-27 00:39:06 +0100

    Prepare 3.3.0 release

--- pmb/__init__.py
+++ pmb/__init__.py
@@ -25,7 +25,7 @@ from .core.context import get_context
 from .commands import run_command
 
 # pmbootstrap version
-__version__ = "3.2.0"
+__version__ = "3.3.0"
 
 # Python version check
 # === CHECKLIST FOR UPGRADING THE REQUIRED PYTHON VERSION ===
