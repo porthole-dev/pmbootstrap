454d5494d7686d5195f142883ce0f0bb2c0b036e
Oliver Smith <ollieparanoid@postmarketos.org>
2025-02-27 01:09:19 +0100

    Prepare 3.3.1 release

--- pmb/config/__init__.py
+++ pmb/config/__init__.py
@@ -30,8 +30,8 @@ apk_keys_path: Path = pmb_src / "pmb/data/keys"
 # (which may contain a vulnerable apk/openssl, and allows an attacker to
 # exploit the system!)
 apk_tools_min_version = {
-    "edge": "2.14.9-r0",
-    "v3.21": "2.14.6-r2",
+    "edge": "2.14.9-r1",
+    "v3.21": "2.14.6-r3",
     "v3.20": "2.14.4-r1",
     "v3.19": "2.14.4-r0",
     "v3.18": "2.14.4-r0",
