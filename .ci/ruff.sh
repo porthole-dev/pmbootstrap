ad3a49cbc9e0ac3682030a73e44f7d9ae1392085
Oliver Smith <ollieparanoid@postmarketos.org>
2025-03-20 21:33:40 +0100

    CI: ruff: use 0.11.0 (Cannot use star annotation)
    Ruff 0.11.1 is incorrectly printing the following types of errors:
    
    pmb/helpers/logging.py:169:34: SyntaxError: Cannot use star annotation on Python 3.10 (syntax was added in Python 3.11)
        |
    169 | def critical(msg: object, *args: str, **kwargs: Any) -> None:
        |                                  ^^^
    170 |     logging.critical(msg, *args, **kwargs)
        |
    
    But the code works fine in Python 3.10 (see pytest-python3.10 CI job).
    This is a known upstream bug, pin to the previous version until it is
    fixed.
    
    Related: https://github.com/astral-sh/ruff/issues/16874
    Part-of: https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/2578

--- .ci/ruff.sh
+++ .ci/ruff.sh
@@ -5,7 +5,9 @@
 if [ "$(id -u)" = 0 ]; then
 	set -x
 	apk -q add py3-pip
-	pip install --break-system-packages --no-warn-script-location ruff
+	# pin to ruff 0.11.0 until this is fixed:
+	# https://github.com/astral-sh/ruff/issues/16874
+	pip install --break-system-packages --no-warn-script-location ruff==0.11.0
 	exec su "${TESTUSER:-build}" -c "sh -e $0"
 fi
 
