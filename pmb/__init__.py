5a1516d474a67915ed55481bf158fad1fa7f39fa
Stefan Hansson <newbyte@postmarketos.org>
2025-05-13 19:42:56 +0200

    Prepare 3.4.0 release

--- pmb/__init__.py
+++ pmb/__init__.py
@@ -25,7 +25,7 @@ from .core.context import get_context
 from .commands import run_command
 
 # pmbootstrap version
-__version__ = "3.3.1"
+__version__ = "3.4.0"
 
 # Python version check
 # === CHECKLIST FOR UPGRADING THE REQUIRED PYTHON VERSION ===
