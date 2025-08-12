porthole-dev/pmbootstrap

commit 34127f579dfcefee33c92da2a532d12b3991e4e7

pmb: support bootimg v3/v4 and `vendor_boot`
Part-of: https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/2630

pmb/flasher/variables.py
@@ -26,10 +26,12 @@ def variables(
     # See also https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/issues/2243
 
     partition_kernel: str | None
+    partition_vendor_boot: str | None
     partition_rootfs: str | None
 
     if method.startswith("fastboot"):
         partition_kernel = deviceinfo.flash_fastboot_partition_kernel or "boot"
+        partition_vendor_boot = deviceinfo.flash_fastboot_partition_vendor_boot or "vendor_boot"
         partition_rootfs = (
             deviceinfo.flash_fastboot_partition_rootfs
             or deviceinfo.flash_fastboot_partition_system
@@ -47,11 +49,17 @@ def variables(
         partition_dtbo = None
     elif method.startswith("mtkclient"):
         partition_kernel = deviceinfo.flash_mtkclient_partition_kernel or "boot"
+        partition_vendor_boot = (
+            deviceinfo.flash_fastboot_partition_vendor_boot or None
+        )  # TODO: is there a default?
         partition_rootfs = deviceinfo.flash_mtkclient_partition_rootfs or "userdata"
         partition_vbmeta = deviceinfo.flash_mtkclient_partition_vbmeta or None
         partition_dtbo = deviceinfo.flash_mtkclient_partition_dtbo or None
     else:
         partition_kernel = deviceinfo.flash_heimdall_partition_kernel or "KERNEL"
+        _partition_vendor_boot = (
+            deviceinfo.flash_heimdall_partition_vendor_boot or None
+        )  # TODO: is there a default name?
         partition_rootfs = (
             deviceinfo.flash_heimdall_partition_rootfs
             or deviceinfo.flash_heimdall_partition_system
@@ -64,6 +72,7 @@ def variables(
         # Only one operation is done at same time so it doesn't matter
         # sharing the arg
         partition_kernel = partition
+        partition_vendor_boot = partition
         partition_rootfs = partition
         partition_vbmeta = partition
         partition_dtbo = partition
@@ -86,6 +95,7 @@ def variables(
         "$IMAGE": "/home/pmos/rootfs/" + device + ".img",
         "$KERNEL_CMDLINE": cmdline_,
         "$PARTITION_KERNEL": partition_kernel,
+        "$PARTITION_VENDOR_BOOT": partition_vendor_boot,
         "$PARTITION_INITFS": deviceinfo.flash_heimdall_partition_initfs or "RECOVERY",
         "$PARTITION_ROOTFS": partition_rootfs,
         "$PARTITION_VBMETA": partition_vbmeta,

pmb/aportgen/linux.py
@@ -31,7 +31,7 @@ def generate_apkbuild(pkgname: str, deviceinfo: Deviceinfo, patches: list[str]) 
             downstreamkernel_package "$builddir" "$pkgdir" "$_carch\" \\
                 "$_flavor" "$_outdir\""""
 
-    if deviceinfo.header_version == "2":
+    if deviceinfo.header_version and int(deviceinfo.header_version) >= 2:
         package += """
 
             make dtbs_install O="$_outdir" ARCH="$_carch" \\

pmb/config/__init__.py
@@ -372,6 +372,7 @@ $IMAGE: Path to the combined boot/rootfs image
 $IMAGE_SPLIT_BOOT: Path to the (split) boot image
 $IMAGE_SPLIT_ROOT: Path to the (split) rootfs image
 $PARTITION_KERNEL: Partition to flash the kernel/boot.img to
+$PARTITION_VENDOR_BOOT: Partition to flash the vendor_boot.img to
 $PARTITION_ROOTFS: Partition to flash the rootfs to
 
 Fastboot specific: $KERNEL_CMDLINE
@@ -385,6 +386,9 @@ flashers: dict[str, dict[str, bool | list[str] | dict[str, list[list[str]]]]] = 
             "list_devices": [["fastboot", "devices", "-l"]],
             "flash_rootfs": [["fastboot", "flash", "$PARTITION_ROOTFS", "$IMAGE"]],
             "flash_kernel": [["fastboot", "flash", "$PARTITION_KERNEL", "$BOOT/boot.img$FLAVOR"]],
+            "flash_vendorboot": [
+                ["fastboot", "flash", "$PARTITION_VENDOR_BOOT", "$BOOT/vendor_boot.img"]
+            ],
             "flash_vbmeta": [
                 # Generate vbmeta image with "disable verification" flag
                 [
@@ -402,6 +406,11 @@ flashers: dict[str, dict[str, bool | list[str] | dict[str, list[list[str]]]]] = 
             ],
             "flash_dtbo": [["fastboot", "flash", "$PARTITION_DTBO", "$BOOT/dtbo.img"]],
             "boot": [["fastboot", "--cmdline", "$KERNEL_CMDLINE", "boot", "$BOOT/boot.img$FLAVOR"]],
+            # Like boot, but stage vendor_boot first
+            "boot_gki": [
+                ["fastboot", "stage", "$BOOT/vendor_boot.img"],
+                ["fastboot", "boot", "$BOOT/boot.img$FLAVOR"],
+            ],
             "flash_lk2nd": [["fastboot", "flash", "$PARTITION_KERNEL", "$BOOT/lk2nd.img"]],
         },
     },

pmb/export/symlinks.py
@@ -21,7 +21,12 @@ def symlinks(folder: Path) -> None:
 
     # File descriptions
     info = {
-        "boot.img": ("Fastboot compatible boot.img file, contains initramfs and kernel"),
+        "boot.img": (
+            "Fastboot compatible boot.img file, contains initramfs with bootimg header <=2 and kernel"
+        ),
+        "vendor_boot.img": (
+            "Fastboot compatible vendor_boot.img file, contains cmdline, initramfs and dtb. Only with bootimg header >= v3"
+        ),
         "dtbo.img": "Fastboot compatible dtbo image",
         "initramfs": "Initramfs",
         "initramfs-extra": "Extra initramfs files in /boot",
@@ -41,6 +46,7 @@ def symlinks(folder: Path) -> None:
     chroot_buildroot = Chroot.buildroot(arch)
     files: list[Path] = [
         path_boot / "boot.img",
+        path_boot / "vendor_boot.img",
         path_boot / "uInitrd",
         path_boot / "uImage",
         path_boot / "dtbo.img",

pmb/flasher/frontend.py
@@ -35,8 +35,21 @@ def kernel(
     if autoinstall:
         pmb.chroot.initfs.build(Chroot(ChrootType.ROOTFS, deviceinfo.codename))
 
+    # deviceinfo.header_version >= 3 == GKI == has / needs vendor_boot
+    is_gki = deviceinfo.header_version and int(deviceinfo.header_version) >= 3
+
     # Check kernel config
     pmb.parse.kconfig.check(flavor, must_exist=False)
+
+    # Do not allow header_version >= 3 for non-fastboot, since the correct way to flash
+    # vendor_boot with other flash methods is unknown as of 2025-08.
+    if not deviceinfo.flash_method == "fastboot" and (
+        deviceinfo.header_version and int(deviceinfo.header_version) >= 3
+    ):
+        raise NonBugError(
+            "deviceinfo_header_version' <= 2 is currently only supported with fastboot. See also:"
+            " <https://wiki.postmarketos.org/wiki/Deviceinfo_reference>"
+        )
 
     # Generate the paths and run the flasher
     if boot:
@@ -44,7 +57,7 @@ def kernel(
         pmb.flasher.run(
             deviceinfo,
             method,
-            "boot",
+            "boot_gki" if is_gki else "boot",
             flavor,
             cmdline=cmdline,
             no_reboot=no_reboot,
@@ -63,6 +76,18 @@ def kernel(
             partition=partition,
             resume=resume,
         )
+        if is_gki:
+            pmb.flasher.run(
+                deviceinfo,
+                method,
+                "flash_vendorboot",
+                flavor,
+                cmdline=cmdline,
+                no_reboot=no_reboot,
+                partition=partition,
+                resume=resume,
+            )
+
     logging.info("You will get an IP automatically assigned to your USB interface shortly.")
     logging.info("Then you can connect to your device using ssh after pmOS has booted:")
     logging.info(f"ssh {get_context().config.user}@{pmb.config.default_ip}")

pmb/flasher/run.py
@@ -5,6 +5,7 @@ import pmb.flasher
 import pmb.chroot.initfs
 import pmb.helpers.args
 from pmb.types import RunOutputTypeDefault
+from pmb.helpers.exceptions import NonBugError
 
 
 def check_partition_blacklist(deviceinfo: Deviceinfo, key: str, value: str) -> None:
@@ -57,11 +58,20 @@ def run(
     # vbmeta flasher requires vbmeta partition to be explicitly specified
     if action == "flash_vbmeta" and not fvars["$PARTITION_VBMETA"]:
         raise RuntimeError(
-            "Your device does not have 'vbmeta' partition"
-            " specified; set"
-            " 'deviceinfo_flash_fastboot_partition_vbmeta'"
-            " or 'deviceinfo_flash_heimdall_partition_vbmeta'"
-            " in deviceinfo file. See also:"
+            "Your device does not have 'vbmeta' partition specified; set"
+            " 'deviceinfo_flash_fastboot_partition_vbmeta' or"
+            " 'deviceinfo_flash_heimdall_partition_vbmeta' in deviceinfo file. See also:"
+            " <https://wiki.postmarketos.org/wiki/Deviceinfo_reference>"
+        )
+
+    if (
+        action == "flash_vendorboot"
+        and deviceinfo.header_version
+        and int(deviceinfo.header_version) <= 2
+    ):
+        raise NonBugError(
+            "'vendor_boot' is only supported with"
+            " 'deviceinfo_header_version' >= 3. See also:"
             " <https://wiki.postmarketos.org/wiki/"
             "Deviceinfo_reference>"
         )

pmb/install/_install.py
@@ -1095,7 +1095,7 @@ def print_flash_info(
         logging.info(f"  {Chroot.rootfs(device) / 'boot/boot.img'}")
         logging.info("  (NOTE: This is not necessary if using a custom bootloader like U-Boot)")
 
-    if "boot" in flasher_actions:
+    if "boot" in flasher_actions or "boot_gki" in flasher_actions:
         logging.info(
             "  (NOTE: " + method + " also supports booting"
             " the kernel/initramfs directly without flashing."

pmb/parse/deviceinfo.py
@@ -123,12 +123,14 @@ class Deviceinfo:
 
     # flash
     flash_heimdall_partition_kernel: str | None = ""
+    flash_heimdall_partition_vendor_boot: str | None = ""
     flash_heimdall_partition_initfs: str | None = ""
     flash_heimdall_partition_rootfs: str | None = ""
     flash_heimdall_partition_system: str | None = ""  # deprecated
     flash_heimdall_partition_vbmeta: str | None = ""
     flash_heimdall_partition_dtbo: str | None = ""
     flash_fastboot_partition_kernel: str | None = ""
+    flash_fastboot_partition_vendor_boot: str | None = ""
     flash_fastboot_partition_rootfs: str | None = ""
     flash_fastboot_partition_system: str | None = ""  # deprecated
     flash_fastboot_partition_vbmeta: str | None = ""
@@ -137,6 +139,7 @@ class Deviceinfo:
     flash_rk_partition_rootfs: str | None = ""
     flash_rk_partition_system: str | None = ""  # deprecated
     flash_mtkclient_partition_kernel: str | None = ""
+    flash_mtkclient_partition_vendor_boot: str | None = ""
     flash_mtkclient_partition_rootfs: str | None = ""
     flash_mtkclient_partition_vbmeta: str | None = ""
     flash_mtkclient_partition_dtbo: str | None = ""

commit e69ee6845353f80ac1d4525cef989fdaea44194a

pmb.flasher.variables: fix typo
_partition_vendor_boot -> partition_vendor_boot, otherwise some of the
flash methods fail with:

ERROR: cannot access local variable 'partition_vendor_boot' where it
is not associated with a value

Fixes: 34127f579dfc ("pmb: support bootimg v3/v4 and `vendor_boot`")

Part-of: https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/2667

pmb/flasher/variables.py
@@ -57,7 +57,7 @@ def variables(
         partition_dtbo = deviceinfo.flash_mtkclient_partition_dtbo or None
     else:
         partition_kernel = deviceinfo.flash_heimdall_partition_kernel or "KERNEL"
-        _partition_vendor_boot = (
+        partition_vendor_boot = (
             deviceinfo.flash_heimdall_partition_vendor_boot or None
         )  # TODO: is there a default name?
         partition_rootfs = (