porthole-dev/pmbootstrap

commit ad1611168f94d5a0b95d86736e162c77f153c3c8

pmb.chroot.init: bootstrap the /usr-merge by creating symlinks
Instead of using a package that will do it for us. The subpackage
approach has been decided against upstream[1]. Indeed, this new
approach fixes some things, like being able to bootstrap without a
binary repository.

This partiall reverts commits 8edb1a0ca1b11bde8e8cd421b175d0587a1d93ba
and e429f90274155e1530b98c58b71ec6e0d2eded66

[1] https://gitlab.alpinelinux.org/alpine/aports/-/merge_requests/85504#note_525558

Part-of: <https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/2680>

pmb/chroot/init.py
@@ -16,7 +16,6 @@ import pmb.helpers.repo
 import pmb.helpers.run
 import pmb.helpers.other
 from pmb.core import Chroot, ChrootType
-from pmb.core.arch import Arch
 from pmb.core.context import get_context
 from pmb.types import PathString
 
@@ -125,22 +124,23 @@ def init(chroot: Chroot) -> None:
     pmb.config.workdir.chroot_save_init(chroot)
 
     pmb.helpers.repo.update(arch)
+    # Create the /usr-merge-related symlinks, which needs to be done manually
+    pmb.helpers.run.root(
+        [
+            "mkdir",
+            "-p",
+            f"{chroot.path}/usr/bin",
+            f"{chroot.path}/usr/sbin",
+            f"{chroot.path}/usr/lib",
+        ]
+    )
+    pmb.helpers.run.root(["ln", "-s", "usr/bin", "usr/sbin", "usr/lib", f"{chroot.path}/"])
     # Install minimal amount of things to get a functional chroot.
     # We don't use alpine-base since it depends on openrc, and neither
     # postmarketos-base, since that's quite big (e.g: contains an init system)
-    cmd: list[PathString] = ["--initdb"]
     pkgs = ["alpine-baselayout", "apk-tools", "busybox", "musl-utils"]
-    # FIXME: The check for a binary repository can be removed once
-    # alpine-baselayout-core is upstreamed to Alpine
-    if (
-        pmb.config.pmaports.read_config().get("supported_usr_merge", False)
-        and arch in Arch.supported_binary()
-    ):
-        # Do the /usr merge! Bootstrapping is done in 2 steps
-        pmb.helpers.apk.run([*cmd, "add", "alpine-baselayout-core"], chroot)
-        pmb.helpers.apk.run(["add", *pkgs], chroot)
-    else:
-        pmb.helpers.apk.run([*cmd, "add", *pkgs], chroot)
+    cmd: list[PathString] = ["--initdb"]
+    pmb.helpers.apk.run([*cmd, "add", *pkgs], chroot)
 
     # Building chroots: create "pmos" user, add symlinks to /home/pmos
     if chroot.type != ChrootType.ROOTFS:

commit 9a059e734fcada0c2f07fb0092230bbb3a64848f

Fix /usr merge breaking stable branches
The /usr merge should only be done if it is supported by the branch, as
indicated in supported_usr_merge in pmaports.cfg. Currently it works in
edge, but not in v25.06 or older.

Without this patch, v25.06 installations currently fail when building
the initramfs, e.g. with qemu-amd64 and the console UI:

12:04:42.337639 hookfiles: unable to process hook file "/usr/share/mkinitfs/files-extra/00-initramfs-extra-base.files": unable to add "/usr/sbin/udevd": getFile: failed to stat file "/usr/sbin/udevd": stat /usr/sbin/udevd: no such file or directory (also tried "/usr/sbin/udevd.zst": stat /usr/sbin/udevd.zst: no such file or directory)

Fixes: ad161116 ("pmb.chroot.init: bootstrap the /usr-merge by creating symlinks")

Part-of: https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/2687

pmb/chroot/init.py
@@ -125,16 +125,17 @@ def init(chroot: Chroot) -> None:
 
     pmb.helpers.repo.update(arch)
     # Create the /usr-merge-related symlinks, which needs to be done manually
-    pmb.helpers.run.root(
-        [
-            "mkdir",
-            "-p",
-            f"{chroot.path}/usr/bin",
-            f"{chroot.path}/usr/sbin",
-            f"{chroot.path}/usr/lib",
-        ]
-    )
-    pmb.helpers.run.root(["ln", "-s", "usr/bin", "usr/sbin", "usr/lib", f"{chroot.path}/"])
+    if pmb.config.pmaports.read_config().get("supported_usr_merge", False):
+        pmb.helpers.run.root(
+            [
+                "mkdir",
+                "-p",
+                f"{chroot.path}/usr/bin",
+                f"{chroot.path}/usr/sbin",
+                f"{chroot.path}/usr/lib",
+            ]
+        )
+        pmb.helpers.run.root(["ln", "-s", "usr/bin", "usr/sbin", "usr/lib", f"{chroot.path}/"])
     # Install minimal amount of things to get a functional chroot.
     # We don't use alpine-base since it depends on openrc, and neither
     # postmarketos-base, since that's quite big (e.g: contains an init system)