b6aaf7d2f5ad263c9fb00fe6836c9218e0324041
Newbyte <newbyte@postmarketos.org>
2025-09-30 15:08:05 +0200

    test.parse.test_cpuinfo: New test
    Part-of: <https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/2692>

---
+++ test/parse/test_cpuinfo.py
@@ -0,0 +1,34 @@
+# Copyright 2025 Stefan Hansson
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from pmb.parse.cpuinfo import arm_big_little_first_group_ncpus
+
+from pathlib import Path
+from typing import Final
+
+
+TESTDIR: Final[Path] = Path(__file__).parent.parent / "data/tests"
+
+
+def test_arm_big_little_first_group_ncpus() -> None:
+    ncpus = arm_big_little_first_group_ncpus()
+
+    # Whether it is None or an int will depend on the host architecture, so let's allow any
+    # reasonable value.
+    assert ncpus is None or ncpus > 0
+
+
+def test_google_grouper_tegra3_cpuinfo() -> None:
+    assert arm_big_little_first_group_ncpus(TESTDIR / "cpuinfo-google-grouper-tegra3.txt") is None
+
+
+def test_google_kukui_krane_mt8183_cpuinfo() -> None:
+    assert arm_big_little_first_group_ncpus(TESTDIR / "cpuinfo-google-kukui-krane-mt8183.txt") == 4
+
+
+def test_oneplus_echilada_sdm845_cpuinfo() -> None:
+    assert arm_big_little_first_group_ncpus(TESTDIR / "cpuinfo-oneplus-enchilada-sdm845.txt") == 4
+
+
+def test_x86_64_cpuinfo() -> None:
+    assert arm_big_little_first_group_ncpus(TESTDIR / "cpuinfo-x86-64-desktop.txt") is None
