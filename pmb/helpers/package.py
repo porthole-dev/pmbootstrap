aee632c724f92d73c632f594c4de71d856eb613b
Pablo Correa Gómez <pabloyoyoista@postmarketos.org>
2026-02-16 10:13:40 +0100

    pmb.helpers.package: also track the original names of the packages
    Similarly to the provides, there are situations where the queued name
    is different to the name of the package. This can happen, for example,
    when the queued name to search is a subpackage, or when it has version
    constraints. Keeping track of those names improves the search
    performance, but also helps break infinite loops.
    
    Without this commit, -dev subpackages could be queued repeatedly if
    multiple packages depend on it, since the package detected is actual
    package and not the subpackage. This commit avoids searching again for
    the -dev subpackage. This can also result in infinite loops in some
    corner cases (usually around bootstrapping) where a dev subpackage
    depends on a subpackage of another file that in turns depends on the
    dev subpackage. This is currently the case with the aports and
    pmaports versions of dbus and systemd-stage0 package.
    
    Also add a test to avoid the infinite loop to be introduced again
    
    Signed-off-by: Pablo Correa Gómez <pabloyoyoista@postmarketos.org>
    Part-of: <https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/2837>

--- pmb/helpers/package.py
+++ pmb/helpers/package.py
@@ -174,18 +174,20 @@ def depends_recurse(pkgname: str, arch: Arch) -> list[str]:
     # Build ret (by iterating over the queue)
     queue = {pkgname}
     ret: set[str] = set()
-    providers: set[str] = set()
+    alternative_names: set[str] = set()
     while len(queue):
-        package = get(queue.pop(), arch)
+        pkgname = queue.pop()
+        package = get(pkgname, arch)
 
         # Add its depends to the queue
         for depend in package.depends:
-            if depend not in ret and depend not in providers:
+            if depend not in ret and depend not in alternative_names:
                 queue.add(depend)
 
         # Add the pkgname (not possible subpkgname) to ret
         ret.add(package.pkgname)
-        providers.update(package.provides)
+        alternative_names.update(package.provides)
+        alternative_names.add(pkgname)
 
     return sorted(ret)
 
--- test/helpers/test_package.py
+++ test/helpers/test_package.py
@@ -2,7 +2,12 @@
 # Copyright 2026 Pablo Correa Gomez
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from pmb.helpers.package import check_version_constraints, remove_operators
+from _pytest.monkeypatch import MonkeyPatch
+
+import pmb.helpers.package
+from pmb.core.arch import Arch
+from pmb.core.package_metadata import PackageMetadata
+from pmb.helpers.package import check_version_constraints, depends_recurse, remove_operators
 
 
 def test_remove_operators() -> None:
@@ -36,3 +41,49 @@ def test_check_version_constraints() -> None:
     assert check_version_constraints("hello-world♻️1.1", "1.0") is True
     assert check_version_constraints("hello-world♻️1.0", "1.0") is True
     assert check_version_constraints("hello-world♻️0.9", "1.0") is True
+
+
+def test_depends_recurse_providers(monkeypatch: MonkeyPatch) -> None:
+    def _get_package(pkgname: str, arch: Arch) -> PackageMetadata:
+        match pkgname:
+            case pkgname if pkgname.startswith("so:libc.musl-"):
+                return PackageMetadata(
+                    [str(arch)], [], "musl", [f"so:libc.musl-{arch}.so.1"], "1.2.5-r21", False
+                )
+            case "musl":
+                return PackageMetadata(
+                    [str(arch)], [], "musl", [f"so:libc.musl-{arch}.so.1"], "1.2.5-r21", False
+                )
+            case "glib":
+                return PackageMetadata(
+                    [str(arch)], [f"so:libc.musl-{arch}.so.1"], "glib", [], "2.31.1-r0", False
+                )
+            case "device-oneplus-fajita":
+                return PackageMetadata([str(arch)], ["glib"], pkgname, [], "34-r0", True)
+            case _:
+                raise AssertionError("This is unreachable code")
+
+    monkeypatch.setattr(pmb.helpers.package, "get", _get_package)
+    depends = depends_recurse("device-oneplus-fajita", Arch.aarch64)
+    assert depends == ["device-oneplus-fajita", "glib", "musl"]
+
+
+def test_depends_recurse_alternative_names(monkeypatch: MonkeyPatch) -> None:
+    def _get_package(pkgname: str, arch: Arch) -> PackageMetadata:
+        match pkgname:
+            case "dbus" | "dbus-dev<99990":
+                return PackageMetadata([str(arch)], ["dbus-libs"], "dbus", [], "1.16.2-r0", False)
+            case "dbus-libs":
+                return PackageMetadata(
+                    [str(arch)], ["systemd-stage0-libs"], "dbus", [], "99991.16.2-r0", True
+                )
+            case "systemd-stage0-libs" | "systemd-stage0":
+                return PackageMetadata(
+                    [str(arch)], ["dbus-dev<99990"], "systemd-stage0", [], "34-r0", True
+                )
+            case _:
+                raise AssertionError("This is unreachable code")
+
+    monkeypatch.setattr(pmb.helpers.package, "get", _get_package)
+    depends = depends_recurse("systemd-stage0", Arch.aarch64)
+    assert depends == ["dbus", "systemd-stage0"]
